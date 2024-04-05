c82fbbbd2eb049effbd1a5a124899e422b087888
vivien <158144861+vivien523@users.noreply.github.com>
2024-04-05 13:47:16 +0800

    script.js

--- script.js
+++ script.js
@@ -32,7 +32,15 @@ submitForm.addEventListener("submit", function(event) {
         document.getElementById("message").value = "";
     });
 
-
 });
+
+<!-- Google tag (gtag.js) -->
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4ZEM912YLQ');
+
 
 
